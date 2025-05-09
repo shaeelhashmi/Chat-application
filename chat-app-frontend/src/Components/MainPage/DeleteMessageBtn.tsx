c6364263bcbde7e27b82d5300708b6f4181d0e1b
shaeelhashmi <shaeelh001@gmail.com>
2025-05-09 20:55:19 +0500

    fixed design

--- chat-app-frontend/src/Components/MainPage/DeleteMessageBtn.tsx
+++ chat-app-frontend/src/Components/MainPage/DeleteMessageBtn.tsx
@@ -13,7 +13,7 @@ export default function DeleteMessageBtn(props:prop) {
             {
               
             <div className={`relative bg-[#0b0b47] bottom-2 right-20  rounded-md text-center ${option?"opacity-100 w-[140px] h-[50px]":"opacity-0 w-0 h-0"} transition-all duration-500 origin-center`}>
-             <button className="text-white text-sm font-light p-2 hover:bg-[#141474] rounded-md transition-all duration-500">
+             <button className={`text-white text-sm font-light p-2 hover:bg-[#141474] rounded-md transition-all duration-500 ${option?"":"hidden"} `}>
                 Delete message
             </button>
             
